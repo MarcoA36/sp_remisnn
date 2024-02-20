2c92dfc36c65592aa99c2affa616c5dcc93106f7
MarcoA366 <marco_95_5@yahoo.com>
2024-02-20 13:11:04 -0300

    20/2

--- src/componentes/FormContacto/FormContacto.jsx
+++ src/componentes/FormContacto/FormContacto.jsx
@@ -1,35 +1,85 @@
-import React from "react";
+import { useRef, useState } from "react";
+import emailjs from "@emailjs/browser";
+// import { useNavigate } from "react-router-dom";
 import "./FormContacto.css";
 
+
+
 const FormContacto = () => {
+  const form = useRef();
+  const [isSuccess, setIsSuccess] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+  // const navigate = useNavigate()
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm("service_6915ubb", "template_63bqo0r", form.current, {
+        publicKey: "LybJfHsngnMNhmfOs",
+      })
+      .then(
+        () => {
+          console.log("SUCCESS!");
+          setIsSuccess(true);
+          // setTimeout(() => {
+          //   navigate("/");
+          // }, 5000);
+
+        },
+        (error) => {
+          console.log("FAILED...", error.text);
+          setErrorMessage("Error al enviar el mensaje. Por favor, inténtalo de nuevo.");
+        }
+      );
+  };
+
   return (
     <div className="form__container">
-      <form>
+    
+      <form ref={form} onSubmit={sendEmail}>
+      <div className="mb-3">
+          <label htmlFor="input-name" className="form-label">
+            Nombre
+          </label>
+          <input
+            type="text"
+            name="user_name"
+            className="form-control"
+            id="input-name"
+            placeholder="tu nombre"
+          />
+        </div>
         <div className="mb-3">
-          <label htmlFor="exampleFormControlInput1" className="form-label">
+          <label htmlFor="input-email" className="form-label">
             Email
           </label>
           <input
             type="email"
+            name="user_email"
             className="form-control"
-            id="exampleFormControlInput1"
+            id="input-email"
             placeholder="correo@gmail.com"
           />
         </div>
         <div className="mb-3">
-          <label htmlFor="exampleFormControlTextarea1" className="form-label">
+          <label htmlFor="input-consulta" className="form-label">
             Consulta
           </label>
           <textarea
             className="form-control"
-            id="exampleFormControlTextarea1"
+            name="user_message"
+            id="input-consulta"
             rows="5"
           ></textarea>
         </div>
+        {isSuccess && <div className="alert alert-success bg-success text-white">¡Mensaje enviado con éxito!</div>}
+      {errorMessage && <div className="alert alert-danger">{errorMessage}</div>}
         <button type="submit" className="btn btn-dark w-100">
           Enviar
         </button>
       </form>
+      
     </div>
   );
 };
