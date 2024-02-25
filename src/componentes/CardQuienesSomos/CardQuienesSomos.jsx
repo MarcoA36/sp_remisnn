a558d9976a4fbc44e0c93ee5f30c96cb18beb9b4
MarcoA366 <marco_95_5@yahoo.com>
2024-02-25 16:05:17 -0300

    quienes somos

--- src/componentes/CardQuienesSomos/CardQuienesSomos.jsx
+++ src/componentes/CardQuienesSomos/CardQuienesSomos.jsx
@@ -10,28 +10,18 @@ const CardQuienesSomos = () => {
       </div>
       <div className="col-lg-6">
         <div className="card__texto text-center">
-          <h2 className="text-dark fs-1">Quiénes Somos</h2>
+          <h2 className="text-dark fs-1">Bienvenidos a NN</h2>
           <h5>
-            NN es un emprendimiento que nace con el fin de
-            brindar una alternativa de calidad a la actual oferta de remises y
-            taxis que hay en nuestra ciudad.
+            Somos un emprendimiento que nace con el fin de ofrecer una
+            alternativa de calidad a la actual oferta de remises y taxis que hay
+            en nuestra ciudad.
           </h5>
           <h5>
-            Creemos entender las claves para poder brindar el mejor servicio a
-            nuestra alcance, y nos esforzamos día a día para superarnos.
+            Entendemos a la perfección las claves para brindar
+            el mejor servicio posible. Nos esforzamos incansablemente día a día
+            para superar las expectativas, poniendo énfasis en la calidad,
+            confiabilidad y satisfacción de nuestros clientes.
           </h5>
-          {/* <h5>
-            Para eso estamos comprometidos a escuchar y responder sugerencias de nuestros clientes.
-          </h5>
-          <h4>etc etc</h4> */}
-          {/* <p>
-            Ademas, estamos comprometidos a escuchar y responder a sugerencias y
-            reclamos, porque creemos que la mejora continua es fundamental..
-          </p>
-          <p>
-            El tiempo de cada cliente es sagrado, por lo que tenemos como foco
-            principal brindar una respuesta rápida. etc etc
-          </p> */}
         </div>
       </div>
       <div className="col-lg-6 d-lg-none">
