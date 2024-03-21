370040f552b9c16c764835de2e16b8221fb0311c
MarcoA366 <marco_95_5@yahoo.com>
2024-03-20 22:56:57 -0300

    serviceWorker

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import * as serviceWorker from './serviceWorker'
 
 //router
 import { BrowserRouter as Router } from "react-router-dom";
@@ -13,3 +14,5 @@ root.render(
     <App />
   </Router>
 );
+
+serviceWorker.register()
